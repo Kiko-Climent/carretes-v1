327933c35ee2a6da6a3b84ac068c05224cae5a34
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-01-20 09:26:17 +0100

    Stylng Hero

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 const Menu = () => {
   return(
     
-    <header className="w-full py-16 px-1 text-sm font-bold text-black fixed top-0 z-10 bg-white">
+    <header className="w-full py-16 px-2 text-sm font-bold text-black fixed top-0 z-10">
     <nav className="flex flex-row justify-between leading-3 tracking-[0.04em]">
       <div className="flex-col items-start">
         <div>Johnny Carretes</div>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,17 @@
-import Image from "next/image";
-
 const Hero = () => {
   return (
-    <section className="h-[calc(100vh-8rem)] flex">
-      <div className="h-full items-end flex inset-0 top-0">
-        <Image
-          src="/images/image00010.jpeg"
-          alt="Johnny Carretes"
-          className="object-cover h-full"
-          fill
-          priority
-        />
+    <section className="flex h-screen">
+      {/* Contenedor principal */}
+      <div className="flex relative h-[calc(100vh-9.75rem)] self-end justify-center px-2 pb-4">
+        {/* Contenedor para bordes redondeados */}
+        <div className="w-full h-full overflow-hidden rounded-lg">
+          <img
+            src="/images/image00010.jpeg"
+            alt="Johnny Carretes"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center text-xs text-white mix-blend-difference">35°18′35″N 24°53′36″E</div>
       </div>
     </section>
   );
